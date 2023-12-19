0731aa46ee6376a780c0c7e33e864861244fe33a
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2023-12-19 18:15:02 +0100

    add client

---
+++ src/ManagedDb.ConsoleClient/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ManagedDb.Core.Features.GetLatestChanges;
+using System.Text.Json;
+
+var command = args.Length > 0 && args[0] == "commit" 
+    ? GetChangesModeEnum.LastCommit
+    : GetChangesModeEnum.MainBranch;
+
+var service = new GetLatestChangesService();
+
+var changes = service.GetChanges(command);
+
+var jsonSerializerOptions = new JsonSerializerOptions
+{
+    WriteIndented = true
+};
+
+var jsonContent = JsonSerializer.Serialize(
+    changes,
+    jsonSerializerOptions);
+
+var fileName = @"changes.json";
+
+if (File.Exists(fileName)) 
+{
+    File.Delete(fileName);
+}
+
+File.WriteAllText(
+    fileName,
+    jsonContent);
