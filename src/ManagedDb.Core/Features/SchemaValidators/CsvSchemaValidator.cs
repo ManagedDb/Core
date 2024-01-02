60d284e99e66389fbb24eaf22efbdc0220bf7298
Shynggyzkhan Olzhabayev <solzhabayev@microsoft.com>
2024-01-02 14:07:22 +0100

    addNewProducts

---
+++ src/ManagedDb.Core/Features/SchemaValidators/CsvSchemaValidator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedDb.Core.Features.SchemaValidators
+{
+    public class CsvSchemaValidator
+    {
+        private readonly ILogger<CsvSchemaValidator> logger;
+
+        public CsvSchemaValidator(
+            ILogger<CsvSchemaValidator> logger)
+        {
+            this.logger = logger;
+        }
+
+        public bool Validate(
+            string pathToCsv, 
+            string pathToEntitySchema) 
+        {
+            this.LogEntityFile(pathToCsv);
+            this.LogEntityFile(pathToEntitySchema);
+
+            return this.InternalValidate(
+                pathToCsv, 
+                pathToEntitySchema);
+        }
+
+        private bool InternalValidate(string pathToCsv, string pathToEntitySchema) 
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LogEntityFile(string pathToCsv) => 
+            this.logger.LogDebug("{csvFile} is exist: {isExist}",
+                pathToCsv,
+                File.Exists(pathToCsv));
+
+        private void LogEntitySchema(string pathToEntitySchema) =>
+            this.logger.LogDebug("{entityFile} is exist: {isExist}",
+                pathToEntitySchema,
+                File.Exists(pathToEntitySchema));
+    }
+}
---
+++ src/ManagedDb.Core/Features/SchemaValidators/EntitySchema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedDb.Core.Features.SchemaValidators
+{
+    public class EntitySchema
+    {
+        public string Name { get; set; }
+
+        public bool? IsRequired { get; set; }
+
+        public EntityField[] Fields { get; set; } = Array.Empty<EntityField>();
+    }
+
+    public class EntityField 
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int? Order { get; set; }
+
+        public string? Format { get; set; }
+    }
+}
