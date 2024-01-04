6bec687d6621a6452e8784b056026cf1460cac28
Shyn Olzhabayev <olzhabaev.sh@outlook.com>
2024-01-04 14:41:24 +0100

    U/shyn/generaete db (#19)
    * update
    
    * fix

---
+++ src/ManagedDb.ConsoleClient/Commands/CreateDbCommand.cs
@@ -0,0 +1,42 @@
+﻿using Cocona;
+using ManagedDb.Core;
+using ManagedDb.Core.Features.GenerateDbs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ManagedDb.ConsoleClient.Commands;
+
+public class CreateDbCommand
+{
+    private readonly DbGenerator dbGenerator;
+    private readonly IOptions<ManagedDbOptions> options;
+    private ILogger<CreateDbCommand> logger;
+
+    public CreateDbCommand(
+        DbGenerator dbGenerator,
+        IOptions<ManagedDbOptions> options,
+        ILogger<CreateDbCommand> logger)
+    {
+        this.dbGenerator = dbGenerator;
+        this.options = options;
+        this.logger = logger;
+    }
+
+    [Command("createdb")]
+    public async Task Handle()
+    {
+        var pathToDataFolder = options.Value.DataFolderPath;
+        var generatedDbPath = options.Value.DbPath;
+
+        this.logger.LogInformation(
+            "Generating db from data folder {0} to {1}", 
+            pathToDataFolder, 
+            generatedDbPath);
+
+        this.dbGenerator.Generate(
+            pathToDataFolder, 
+            generatedDbPath);
+
+        await Task.CompletedTask;
+    }
+}
---
+++ tests/units/ManagedDb.Core.Units/DbGenerators/DbGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using ManagedDb.Core.Features.GenerateDbs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedDb.Core.Units.DbGenerators
+{
+    [TestClass]
+    public class DbGeneratorTests
+    {
+        [TestMethod]
+        public void GenerateDb_Should_Work() 
+        {
+            var pathToDataFolder = @"D:\Repositories\ManagedDb\Core\data";
+            var dbExportPath = @"D:\Repositories\ManagedDb\Core\data\mdb.db";
+
+            var dbGenerator = new DbGenerator();
+            dbGenerator.Generate(pathToDataFolder, dbExportPath);
+        }
+    }
+}
