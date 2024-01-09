5735f325b539e002b5515f5bd7eadf514aa4aca7
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2024-01-09 22:06:54 +0100

    fix

--- src/ManagedDb.Core/Features/DataProxyCreators/EdmGenerator.cs
+++ src/ManagedDb.Core/Features/DataProxyCreators/EdmGenerator.cs
@@ -1,27 +1,26 @@
-﻿
-using Microsoft.OData.Edm;
-using Microsoft.OData.ModelBuilder;
-using System.Reflection;
+﻿//using Microsoft.OData.Edm;
+//using Microsoft.OData.ModelBuilder;
+//using System.Reflection;
 
-namespace ManagedDb.Core.Features.DataProxyCreators;
+//namespace ManagedDb.Core.Features.DataProxyCreators;
 
-public static class EdmGenerator
-{
-    public static IEdmModel GetEdmModel() 
-    {
-        var modelTypes = Assembly.GetExecutingAssembly().GetTypes()
-            .Where(t => t.IsClass && t.IsSubclassOf(typeof(MdbBaseEntity)));
+//public static class EdmGenerator
+//{
+//    public static IEdmModel GetEdmModel() 
+//    {
+//        var modelTypes = Assembly.GetExecutingAssembly().GetTypes()
+//            .Where(t => t.IsClass && t.IsSubclassOf(typeof(MdbBaseEntity)));
 
-        // odata start
-        var mb = new ODataConventionModelBuilder();
+//        // odata start
+//        var mb = new ODataConventionModelBuilder();
         
-        foreach(var modelType in modelTypes)
-        {
-            mb.AddEntitySet(
-                modelType.Name, 
-                mb.AddEntityType(modelType));
-        }
+//        foreach(var modelType in modelTypes)
+//        {
+//            mb.AddEntitySet(
+//                modelType.Name, 
+//                mb.AddEntityType(modelType));
+//        }
 
-        return mb.GetEdmModel();
-    }
-}
+//        return mb.GetEdmModel();
+//    }
+//}
--- src/ManagedDb.Core/Features/DataProxyCreators/ODataBaseController.cs
+++ src/ManagedDb.Core/Features/DataProxyCreators/ODataBaseController.cs
@@ -1,24 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.OData.Query;
-using Microsoft.AspNetCore.OData.Routing.Controllers;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.OData.Query;
+//using Microsoft.AspNetCore.OData.Routing.Controllers;
 
-namespace ManagedDb.Core.Features.DataProxyCreators;
+//namespace ManagedDb.Core.Features.DataProxyCreators;
 
-[Route("odata/[controller]")]
-public abstract class ODataBaseController<TEntity> : ODataController
-    where TEntity : MdbBaseEntity
-{
-    private readonly ApplicationDbContext dbContext;
+//[Route("odata/[controller]")]
+//public abstract class ODataBaseController<TEntity> : ODataController
+//    where TEntity : MdbBaseEntity
+//{
+//    private readonly ApplicationDbContext dbContext;
 
-    public ODataBaseController(ApplicationDbContext dbContext)
-    {
-        this.dbContext = dbContext;
-    }
+//    public ODataBaseController(ApplicationDbContext dbContext)
+//    {
+//        this.dbContext = dbContext;
+//    }
 
-    [HttpGet]
-    [EnableQuery]
-    protected IQueryable<TEntity> GetQueryable()
-    {
-        return this.dbContext.Set<TEntity>();
-    }
-}
+//    [HttpGet]
+//    [EnableQuery]
+//    protected IQueryable<TEntity> GetQueryable()
+//    {
+//        return this.dbContext.Set<TEntity>();
+//    }
+//}
