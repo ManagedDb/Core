bd0dbe2077c32c5551ac2826482e803ebafe2415
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2024-01-13 20:39:41 +0100

    clear

---
+++ src/ManagedDb.Proxies/Models/Models.Debug.cs
@@ -0,0 +1,16 @@
+﻿using ManagedDb.Dtos.Models;
+
+namespace ManagedDb.Proxies.Models;
+
+#if DEBUG
+
+public class product : MdbBaseEntity 
+{
+    public string title { get; set; }
+
+    public string category { get; set; }
+
+    public int price { get; set; }
+}
+
+#endif
